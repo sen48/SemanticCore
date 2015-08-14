0a9fc5e1c48946f2b54c77f53b1a3ca3bfcbb0f6
sen48 <sen48@bk.ru>
2015-08-14 11:19:39 +0600

    5

--- aquarel/__init__.py
+++ aquarel/__init__.py
@@ -4,19 +4,34 @@ import text_analisys
 import search_query.serps as wrs
 from search_query.content import WebPage
 
+def _get_par(word, morph):
+    tok = re.sub(r'[^\w\s]+|[\d]+|•', r' ', word).strip().lower()
+    return morph.parse(tok)[0]
+
+
+class SpecialWord:
+    """
+    sep: разделитель - это символ между текущим словом и следующим
+    color: цвет - это строка начинающаяся с # а дальше 16-тиричный код цвета
+    title:
+    """
+    def __init__(self, word, morph, fdist, max_freq, sep=' '):
+        par = _get_par(word, morph)
+        color, title = color_word(par, fdist, max_freq)
+        self.word = word
+        self.color = color
+        self.title = title
+        self.sep = sep
+
 
 def colorize_text(fdist, text, morph):
-
     """
-    Расскрашивает текст text, в соответствии с частотами слов fdist.
-    Серым – будут выделены служебные части речи (предлоги, частицы, наречия) и стоп-слова.
-    Для остальных слов градиентом от красного до зеленого будет обозначена частота.
-    Зеленый – частотное слово, красный – нет.
 
     :param fdist:
-    :ыек text:
+    :param text:
     :param morph:
-    :return:
+    :return:  текст с html разметкой
+
     """
 
     text = text.replace('•', ' ').replace('\n', ' ')
@@ -27,15 +42,14 @@ def colorize_text(fdist, text, morph):
     for word in text.split(' '):
         if '-' in word:
             for w in word.split('-')[:-1]:
-                res.append(color_word(w, fdist, morph, max_freq, sep='-'))
-            res.append(color_word(word.split('-')[-1], fdist, morph, max_freq))
+                res.append(SpecialWord(w,  morph, fdist, max_freq, '-'))
+            res.append(SpecialWord(word.split('-')[-1],  morph, fdist, max_freq))
         else:
-            res.append(color_word(word, fdist, morph, max_freq))
-    return res
+            res.append(SpecialWord(word, morph, fdist, max_freq))
+    return make_html(res)
 
 
 def colorable(par):
-
     """
     если рассматриваемое слово служебная часть речи (предлоги, частицы, наречия) иил стоп-слово возвтащает FALSE
     Инече TRUE
@@ -45,13 +59,20 @@ def colorable(par):
     return par.tag.is_productive() and par.normal_form not in text_analisys.stop_words and par.tag.POS != 'ADVB'
 
 
-def color_word(word, fdist, morph, max_freq, sep=' '):
-    tok = re.sub(r'[^\w\s]+|[\d]+|•', r' ', word).strip().lower()
+def color_word(par, fdist, max_freq):
+    """
 
-    par = morph.parse(tok)[0]
+    :param par:
+    :param fdist:
+    :param max_freq:
+    :return: color: строка начинающаяся с # а дальше 16-тиричный код цвета
+             title: строка состоящая из частоты слова(опционально, если слово раскрашивается в оттенок зеленого),
+                    нормальной формы и части речи
+    """
+
     tok = par.normal_form
     if len(tok) == 0:
-        return ['', "#010101", ' ', sep]
+        return "#010101", ' '
     elif tok not in fdist.keys():
         if not colorable(par):
             color = "#010101"
@@ -60,14 +81,20 @@ def color_word(word, fdist, morph, max_freq, sep=' '):
             color = "#ff0000"
             title = "{} {}".format(tok, par.tag.POS)
     else:
-        green = round(100+155*fdist.get(tok) / max_freq)
-        color = "#{0:02x}{1:02x}{2}".format(255-green, green, '00',)
-        title = "{:003.4f} {} {}".format(fdist.get(tok), tok,  par.tag.POS)
-    return [word, color, title, sep]
+        green = round(100 + 155 * fdist.get(tok) / max_freq)
+        color = "#{0:02x}{1:02x}{2}".format(255 - green, green, '00', )
+        title = "{:003.4f} {} {}".format(fdist.get(tok), tok, par.tag.POS)
+    return color, title
 
 
+def make_html(specialwords):
+    html = ''
+    for sw in specialwords:
+        html += '<font color = {} title = {}>{}</font>{}'.format(sw.color, sw.title, sw.word, sw.sep)
+    return html
 
-def colorize(queries, analyzed_text='', region=213, full=False):
+
+def colorize(queries, analyzed_text='', region=213):
     """
     Серым – будут выделены служебные части речи (предлоги, частицы, наречия).
     Для остальных слов градиентом от красного до зеленого будет обозначена степень соответствия запросу.
@@ -78,7 +105,6 @@ def colorize(queries, analyzed_text='', region=213, full=False):
     :param queries: Список запросов
     :param analyzed_text: Текст, который нужно раскрасить
     :param region: регион запроса
-    :param full:  Если TRUE то при анализе учитывются сквозные блоки страниц ТОП10
     :return: color_text - текст с html разметкой,
              most_comm - список наиболее популярных словоформ,
              coll - список наиболее попуолярных словосочетаний
@@ -87,11 +113,11 @@ def colorize(queries, analyzed_text='', region=213, full=False):
 
     collection = []
     for query in queries:
-        collection += get_texts(query, region, full)
+        collection += get_texts(query, region)
 
     fdist, doc_dist, morph = text_analisys.words_freq(collection, normalize=True)
 
-    color_text = color_text (doc_dist, analyzed_text, morph)
+    color_text = colorize_text(doc_dist, analyzed_text, morph)
     most_comm = [str(word[0]) for word in fdist.most_common(20) if str(word[0]) != '']
 
     coll = ', '.join(text_analisys.collocations(collection))
@@ -100,14 +126,21 @@ def colorize(queries, analyzed_text='', region=213, full=False):
         if k == '':
             continue
         lang_freq = text_analisys.CF[k] if k in text_analisys.CF.keys() else 2
-        weirdness[k] = fdist.get(k)/lang_freq
+        weirdness[k] = fdist.get(k) / lang_freq
     weird = sorted(weirdness.keys(), key=lambda k: weirdness[k], reverse=True)[:50]
 
     return color_text, most_comm, coll, weird
 
 
+def get_texts(query, region):
 
-def get_texts(query, region, full, ):
+    """
+    Извлекает тексты ТОП10 по заданному запросу
+    :param query: запрос
+    :param region: регион
+    :return: список текстов.
+    """
+
     serp_items = wrs.read_serp(query, region, 10)
     pgs = []
     for item in serp_items:
@@ -119,3 +152,25 @@ def get_texts(query, region, full, ):
             continue
 
     return [text_analisys.Readable(p.html()).title() + text_analisys.Readable(p.html()).text() for p in pgs]
+
+if __name__ == "__main__":
+    TEXT = '''В интернет-магазине «Троицкая книга» Вы можете в считанные минуты купить облачения и одежду. Церковные ткани,
+    используемые для пошива изделий, представлены в соответствующем разделе.
+    Наш интернет-магазин располагает широким выбором готовых богослужебных облачений (иерейские и диаконские облачения,
+     митры, камилавки, требные комплекты, стихари), а также повседневной священнической и монашеской одежды (рясы, подрясники).
+     Кроме того, у нас Вы найдете безрукавки, брюки и рубашки.
+    Если нужного Вам размера не оказалось в наличии или у вас возникли трудности с подбором необходимых тканей и размера,
+    сразу же звоните нам – мы с радостью Вас проконсультируем и поможем подобрать нужный товар. А если Вы живете в Москве,
+    то лучше приходите в наш магазин. Лично ознакомьтесь с ассортиментом, примерьте понравившиеся изделия и сделайте конечный выбор.
+    Мы также предлагаем возможность заказать индивидуальный пошив изделия для православных священников. Облачения и одежда
+    шьются в нашей мастерской Подворья в Москве в течение 3-х недель, доставляются даже в самые удаленные уголки России и мира.
+    Облачения и одежда православного священника
+
+    Мы производим священнические облачения и одежду для священнослужителей и монахов в собственной мастерской уже 15 лет.
+    В нашем ассортименте представлены ткани на любой вкус и кошелек. Вы можете выбрать православные облачения как из традиционных тканей
+    (шелк, парча русская, парча греческая), так и из более редких. Для пошива изделий мы используем хорошие лекала, поэтому,
+    если Вы правильно указали свои параметры, можете быть уверены, что купленная вещь отлично сядет на Вас.'''
+
+    QUERIES = ["адронный коллайдер", "бозон Хиггса"]
+
+    print(colorize(QUERIES, analyzed_text=TEXT, region=213)[0])
